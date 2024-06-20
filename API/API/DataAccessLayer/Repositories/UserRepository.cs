c3e73d389f70bf14c5e5e4ee4abc814ae1c1da93
HONOR <HONOR@LAPTOP-F9JVG7BL>
2024-06-20 17:36:18 +0300

    move changes from previous branche

---
+++ API/API/DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,65 @@
+﻿using API.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.DataAccessLayer.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private ShopdbContext _dbContext;
+
+        public UserRepository(ShopdbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> Create(User item)
+        {
+            await _dbContext.AddAsync(item);
+            await _dbContext.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            int res = await _dbContext.Users.Where(u => u.Id == id).ExecuteDeleteAsync();
+            await _dbContext.SaveChangesAsync();
+            return res;
+        }
+
+        public async Task<User?> Get(int id)
+        {
+            var res = await _dbContext.Users.Where(u => u.Id == id).FirstOrDefaultAsync();
+            return res;
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            return _dbContext.Users.ToListAsync();
+        }
+
+        public async Task<User?> Update(User item)
+        {
+            var res = await _dbContext.Users.Where(u => u.Id == item.Id).FirstOrDefaultAsync();
+            User u;
+            if (res == null)
+            {
+                return res;
+            }
+            else
+            {
+                u = res;
+                if (item.Email != null)
+                {
+                    u.Email = item.Email;
+                }
+                if (item.Password != null)
+                {
+                    u.Password = item.Password;
+                }
+                _dbContext.Update(u);
+                await _dbContext.SaveChangesAsync();
+                return u;
+            }
+        }
+    }
+}
---
+++ API/API/DataAccessLayer/ShopdbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.DataAccessLayer.Models;
+using System.Numerics;
+using System.Reflection;
+
+namespace API.DataAccessLayer
+{
+
+    public class ShopdbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        string login = "";
+        string password = "";
+
+        public ShopdbContext(DbContextOptions<ShopdbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .UseMySql($"host=localhost; database=shopdb; port=3306; username={login}; password={password}", new MySqlServerVersion(new Version(8, 0, 35)));
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
