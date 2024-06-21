21ac1c6c8e07ff1de12d2746432ba61e25a09642
dartderden <158480745+dartderden@users.noreply.github.com>
2024-06-21 14:15:28 +0300

    Update OrderDto.cs

--- API/API/DTOs/OrderDto.cs
+++ API/API/DTOs/OrderDto.cs
@@ -5,4 +5,5 @@ public record OrdersDto
     public int Id { get; set; }
     public DateTime OrderDate { get; set; }
     public int UserId { get; set; }
+    public List<OrderItemDto>? OrderItems { get; set; }
 }
