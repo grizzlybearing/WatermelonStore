0842b14573705ba4b597e051835412ccd7e7999f
dartderden <158480745+dartderden@users.noreply.github.com>
2024-06-21 14:14:52 +0300

    Update UserDto.cs

--- API/API/DTOs/UserDto.cs
+++ API/API/DTOs/UserDto.cs
@@ -1,9 +1,5 @@
-using System.ComponentModel.DataAnnotations;
 public class UserDto
 {
 	public int Id { get; set; }
-	[EmailAddress]
 	public string Email { get; set; }
-	[Required]
-	public string Password { get; set; }
 }
