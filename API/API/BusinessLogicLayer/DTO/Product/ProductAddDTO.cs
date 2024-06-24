16cd3791fc980dfad4fd43359848c6d9170d4a8e
SeliukOlga225 <Seliuk.O.V.115@gmail.com>
2024-06-24 11:06:16 +0300

    fixes

--- API/API/BusinessLogicLayer/DTO/Product/ProductAddDTO.cs
+++ API/API/BusinessLogicLayer/DTO/Product/ProductAddDTO.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int CategoryId { get; set; }
+        public Guid CategoryId { get; set; }
 
     }
 }
--- API/API/BusinessLogicLayer/Validators/Product/ProductAddDTOValidator.cs
+++ API/API/BusinessLogicLayer/Validators/Product/ProductAddDTOValidator.cs
@@ -12,8 +12,8 @@ namespace API.BusinessLogicLayer.Validators.Product
                     .MaximumLength(40).WithMessage("Name is too long.")
                     .MinimumLength(6).WithMessage("Name is too short.");
             RuleFor(x => x.Description)
-                    .MaximumLength(100).WithMessage("Name is too long.")
-                    .MinimumLength(20).WithMessage("Name is too short.");
+                    .MaximumLength(100).WithMessage("Description is too long.")
+                    .MinimumLength(20).WithMessage("Description is too short.");
             RuleFor(x => x.Price)
                     .NotEmpty().WithMessage("Price is required.");
             RuleFor(x => x.CategoryId)
